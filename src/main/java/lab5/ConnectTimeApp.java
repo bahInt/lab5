7d468a14ed37153fd406ee572e555ee2ce53dfac
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-24 00:54:56 +0300

    Scripted auto-commit on change (2021-12-24 00:54:56) by gitwatch.sh

--- src/main/java/lab5/ConnectTimeApp.java
+++ src/main/java/lab5/ConnectTimeApp.java
@@ -1,9 +1,32 @@
 package lab5;
 
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.stream.ActorMaterializer;
+
 import java.io.IOException;
 
 public class ConnectTimeApp {
     public static void main(String[] args) throws IOException {
-        
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов
+        метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress
+                RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdown
+        when done
     }
 }

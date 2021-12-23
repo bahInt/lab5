421acaad10ccafd3da09811612c047b04bda5212
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-24 01:50:58 +0300

    Scripted auto-commit on change (2021-12-24 01:50:58) by gitwatch.sh

--- src/main/java/lab5/CasherActor.java
+++ src/main/java/lab5/CasherActor.java
@@ -3,10 +3,11 @@ package lab5;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class CasherActor extends AbstractActor {
-    private final Map<String, Float> cash = new
+    private final Map<String, Float> cash = new HashMap<>();
 
     public Receive createReceive() {
         return ReceiveBuilder.create()

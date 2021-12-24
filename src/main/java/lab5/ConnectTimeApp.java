2636ec7901e3663b5531b087b5bc368d42d8829f
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-24 23:07:43 +0300

    Scripted auto-commit on change (2021-12-24 23:07:43) by gitwatch.sh

--- src/main/java/lab5/ConnectTimeApp.java
+++ src/main/java/lab5/ConnectTimeApp.java
@@ -79,7 +79,7 @@ public class ConnectTimeApp {
                         }))
                 .map((r) -> {
                     casher.tell(new StorageMessage(r.first(), r.second()), ActorRef.noSender());
-                    return HttpResponse.create();
+                    return HttpResponse.create().withEntity();
                 });
 
     }

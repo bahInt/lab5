5d3a565fefafc6ca43ddab606b6e4ec9b494bff9
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-24 22:46:33 +0300

    Scripted auto-commit on change (2021-12-24 22:46:33) by gitwatch.sh

--- src/main/java/lab5/StorageMessage.java
+++ src/main/java/lab5/StorageMessage.java
@@ -1,8 +1,19 @@
 package lab5;
 
 public class StorageMessage {
+    private final String url;
+    private final float avgTine;
+
     public StorageMessage(String url, float avgTime) {
         this.url = url;
         this.avgTine = avgTime;
+    }
+
+    public float getAvgTine() {
+        return avgTine;
+    }
+
+    public String getUrl() {
+        return url;
     }
 }

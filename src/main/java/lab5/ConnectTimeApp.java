e32c61307351d55dc62ed4abd6df18e91b13cce2
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-24 18:00:49 +0300

    Scripted auto-commit on change (2021-12-24 18:00:49) by gitwatch.sh

--- src/main/java/lab5/ConnectTimeApp.java
+++ src/main/java/lab5/ConnectTimeApp.java
@@ -56,7 +56,7 @@ public class ConnectTimeApp {
                 })
                 .mapAsync(2, (Pair<String, Integer> p) ->
                         Patterns.ask(casher, p.first(), TIMEOUT).thenCompose((Object t) -> {
-                            if((float) t >= 0) return ;
+                            if((float) t >= 0) return CompletableFuture.completedFuture();
                             return Source.from();
                         }))
                 .map();
